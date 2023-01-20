392068e6c6b94d35d0b3477311371397cc599ffa
zeeshanromi00 <zkromeo6666@gmail.com>
2023-01-20 16:55:39 +0500

    file uploads in Add file page

--- src/components/AddFile/AddFile.js
+++ src/components/AddFile/AddFile.js
@@ -1,13 +1,25 @@
-import React from "react";
+import React,{useState} from "react";
 import "./AddFile.css"
 import {GrAddCircle} from 'react-icons/gr';
 
 export default function (){
+    const [files,setFiles]=useState('')
+const fileUpload =(e)=>{
+let file = e.target.files[0]
+console.log('data',file)
+setFiles(file)
+
+}
     return(
 
         <div>
              <span className="heading"><h4> ADD  A SINGAPORE BIZEFILE</h4></span> 
-    <div className="icone"><GrAddCircle /></div>
+             <input className="hid" id="file_data" type="file" onChange={fileUpload} />
+    <div className="icone" ><label className="for" htmlFor="file_data"><GrAddCircle /></label>
+             {
+                files ? <h6>File Added Successfully</h6>:null
+             }
+    </div>
         </div>
     )
 }
